06c0935be9d12ca69a14309806af32c58ebdaac7
workcom0 <90753448+workcom0@users.noreply.github.com>
2022-09-16 14:03:16 +0900

    S07P22D107-226 MemberRepository으로 파일 이름 변경

--- ZirraForming/server/src/main/java/com/ssafy/server/domain/repository/MemberRepository.java
+++ ZirraForming/server/src/main/java/com/ssafy/server/domain/repository/MemberRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemberRepo extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findByEmail(String email);
 }
