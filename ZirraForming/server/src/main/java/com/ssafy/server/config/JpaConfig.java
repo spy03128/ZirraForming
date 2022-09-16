48459cd45530ee116354665a85fb2c0360ca314a
workcom0 <90753448+workcom0@users.noreply.github.com>
2022-09-16 19:41:00 +0900

    S07P22D107-214 JpaConfig 수정

--- ZirraForming/server/src/main/java/com/ssafy/server/config/JpaConfig.java
+++ ZirraForming/server/src/main/java/com/ssafy/server/config/JpaConfig.java
@@ -1,6 +1,5 @@
 package com.ssafy.server.config;
 
-import com.ssafy.server.config.properties.AppProperties;
 import com.ssafy.server.config.properties.CorsProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +8,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @EnableConfigurationProperties({
         CorsProperties.class,
-        AppProperties.class
 })
 @Configuration
 public class JpaConfig {
