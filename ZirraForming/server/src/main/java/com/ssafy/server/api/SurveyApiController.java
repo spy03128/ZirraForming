eaff2d4f2794f66d98819d6337618c62dc9f46dd
권도현 <tobeykwon@gmail.com>
2022-09-18 03:37:18 +0900

    S07P22D107-271 SurveyApiController 환경유형검사 결과 API 추가

--- ZirraForming/server/src/main/java/com/ssafy/server/api/SurveyApiController.java
+++ ZirraForming/server/src/main/java/com/ssafy/server/api/SurveyApiController.java
@@ -1,12 +1,14 @@
 package com.ssafy.server.api;
 
-import com.ssafy.server.api.dto.SurveyResponse;
-import com.ssafy.server.domain.repository.SurveyRepository;
+import com.ssafy.server.api.dto.survey.SurveyDto;
+import com.ssafy.server.api.dto.survey.SurveyTestResultResponse;
+import com.ssafy.server.domain.service.SurveyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,11 +19,18 @@ import java.util.stream.Collectors;
 @RestController
 public class SurveyApiController {
 
-    private final SurveyRepository surveyRepository;
+    private final SurveyService surveyService;
 
     @GetMapping(value = "/survey", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<SurveyResponse>> getProblemList() {
-        List<SurveyResponse> results = surveyRepository.findAll().stream().map(SurveyResponse::new).collect(Collectors.toList());
+    public ResponseEntity<List<SurveyDto>> getProblemList() {
+        List<SurveyDto> results = surveyService.findAll().stream().map(SurveyDto::new).collect(Collectors.toList());
         return ResponseEntity.ok(results);
     }
+
+    @GetMapping(value = "/charactor", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<SurveyTestResultResponse> getSurveyTestResult(@RequestParam(value = "name") String name) {
+        SurveyTestResultResponse result = new SurveyTestResultResponse(surveyService.getSurveyTestResult(name));
+        return ResponseEntity.ok(result);
+    }
+
 }
