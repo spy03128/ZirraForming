1a705a680bc332ee605d3e089a8b61d3640da619
권도현 <tobeykwon@gmail.com>
2022-09-18 02:47:14 +0900

    S07P22D107-270 SurveyResponse 환경유형검사 문제 조회용 DTO 클래스 추가

---
+++ ZirraForming/server/src/main/java/com/ssafy/server/api/dto/SurveyResponse.java
@@ -0,0 +1,33 @@
+package com.ssafy.server.api.dto;
+
+import com.ssafy.server.domain.entity.Survey;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+public class SurveyResponse {
+    private final String question;
+    private final String answer1;
+    private final String answer2;
+    private final List<Integer> weight1;
+    private final List<Integer> weight2;
+    private final String imgUrl;
+
+    public SurveyResponse(Survey survey) {
+        this.question = survey.getQuestion();
+        this.answer1 = survey.getAnswer1();
+        this.answer2 = survey.getAnswer2();
+        this.weight1 = convertWeightToList(survey.getWeight1());
+        this.weight2 = convertWeightToList(survey.getWeight2());
+        this.imgUrl = survey.getImgUrl();
+    }
+
+    private List<Integer> convertWeightToList(String weight) {
+        return Arrays.asList(weight.split(",")).stream().map(x -> Integer.parseInt(x)).collect(Collectors.toList());
+    }
+}
