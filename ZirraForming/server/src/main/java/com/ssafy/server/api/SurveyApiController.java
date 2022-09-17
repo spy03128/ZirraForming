0b9c0ca4c9e34fbc049205a34a16dc3c22ac53c0
권도현 <tobeykwon@gmail.com>
2022-09-18 04:14:54 +0900

    S07P22D107-272 SurveyApiController 환경유형검사 결과 저장 API 추가

--- ZirraForming/server/src/main/java/com/ssafy/server/api/SurveyApiController.java
+++ ZirraForming/server/src/main/java/com/ssafy/server/api/SurveyApiController.java
@@ -1,15 +1,15 @@
 package com.ssafy.server.api;
 
+import com.ssafy.server.api.dto.common.ResultDto;
 import com.ssafy.server.api.dto.survey.SurveyDto;
 import com.ssafy.server.api.dto.survey.SurveyTestResultResponse;
+import com.ssafy.server.api.dto.survey.SurveyResultSaveRequest;
 import com.ssafy.server.domain.service.SurveyService;
+import com.ssafy.server.util.AuthenticationUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,6 +20,7 @@ import java.util.stream.Collectors;
 public class SurveyApiController {
 
     private final SurveyService surveyService;
+    private final AuthenticationUtil authenticationUtil;
 
     @GetMapping(value = "/survey", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<SurveyDto>> getProblemList() {
@@ -31,6 +32,13 @@ public class SurveyApiController {
     public ResponseEntity<SurveyTestResultResponse> getSurveyTestResult(@RequestParam(value = "name") String name) {
         SurveyTestResultResponse result = new SurveyTestResultResponse(surveyService.getSurveyTestResult(name));
         return ResponseEntity.ok(result);
+    }
+
+    @PostMapping(value = "/surveyresult", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResultDto> saveSurveyResult(@RequestBody SurveyResultSaveRequest request) {
+        Long memberId = authenticationUtil.getLoginMemberId();
+        surveyService.updateMemberCharacterType(memberId, request.getCharacterName());
+        return ResponseEntity.ok(ResultDto.of("환경유형 테스트 결과가 저장되었습니다."));
     }
 
 }
