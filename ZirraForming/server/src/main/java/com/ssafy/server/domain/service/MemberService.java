ce79ecb6ae29fab181429f346524bd7ce9a2c867
workcom0 <90753448+workcom0@users.noreply.github.com>
2022-09-18 16:48:18 +0900

    S07P22D107-277 회원 탈퇴 Service 추가

--- ZirraForming/server/src/main/java/com/ssafy/server/domain/service/MemberService.java
+++ ZirraForming/server/src/main/java/com/ssafy/server/domain/service/MemberService.java
@@ -6,6 +6,8 @@ import com.ssafy.server.domain.repository.MemberRepository;
 import com.ssafy.server.oauth.utils.CookieUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +21,7 @@ import javax.servlet.http.HttpServletResponse;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final RedisTemplate<String, String> redisTemplate;
 
     public void logout(HttpServletRequest request, HttpServletResponse response){
         CookieUtil.deleteCookie(request, response, "refreshToken");
@@ -40,5 +43,32 @@ public class MemberService {
 
         // 멤버 닉네임 업데이트
         findMember.updateNickname(nickname);
+    }
+
+    public boolean deleteMember(Long memberId){
+
+        try {
+            // 멤버 조회
+            Member findMember = memberRepository.findById(memberId)
+                    .orElseThrow(() -> new MemberNotFountException(memberId));
+
+            // 멤버 삭제
+            memberRepository.deleteById(memberId);
+
+            // 리프레시 토큰 삭제
+            deleteRefreshToken(memberId);
+
+        }catch (MemberNotFountException e){
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    public void deleteRefreshToken(Long memberId){
+        String key = "refreshToken:" + String.valueOf(memberId);
+        SetOperations<String, String> setOperations = redisTemplate.opsForSet();
+        if(setOperations.size(key)!=0) setOperations.pop(key);
     }
 }
