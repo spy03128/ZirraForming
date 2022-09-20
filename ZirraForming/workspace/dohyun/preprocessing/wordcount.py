e9c1f0e025cab462631f7f581f63ac5bfe6b60f3
권도현 <tobeykwon@gmail.com>
2022-09-20 14:33:39 +0900

    S07P22D107-254 wordcount.py 전처리 코드 추가

---
+++ ZirraForming/workspace/dohyun/preprocessing/wordcount.py
@@ -0,0 +1,35 @@
+import findspark
+import pyspark
+from pyspark.sql import SparkSession
+
+
+def make_spark_connection() -> SparkSession:
+    return SparkSession.builder.getOrCreate()
+
+
+def read_text_file(spark: SparkSession, filepath: str) -> pyspark.RDD:
+    rdd = spark.sparkContext.textFile(f'hdfs://cluster:8020/user/j7d107/{filepath}.txt')
+    rdd.filter(lambda x: x != "")
+    return rdd
+
+
+def read_csv_file(spark: SparkSession, filepath: str) -> pyspark.sql.dataframe:
+    return spark.read.csv(f'hdfs://cluster:8020/user/j7d107/{filepath}.csv')
+
+
+def map_phaze(rdd):
+    return rdd.flatMap(lambda x: x.split())
+
+
+def reduce_phaze(rdd):
+    return rdd.map(lambda x: (x, 1)).groupByKey().mapValues(sum).sortByKey(True)
+
+
+findspark.init()
+spark = make_spark_connection()
+rdd = read_text_file(spark, "wordcount-data")
+
+map_result = map_phaze(rdd)
+reduce_result = reduce_phaze(map_result)
+
+print(reduce_result.collect())
