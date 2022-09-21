01065fc38c5ce782b56cb767834cbd9ce09445ef
권도현 <tobeykwon@gmail.com>
2022-09-22 01:29:20 +0900

    S07P22D107-437 oecd_airpollution.py 수정
    코드 오류 수정

--- ZirraForming/workspace/dohyun/preprocessing/oecd_airpollution.py
+++ ZirraForming/workspace/dohyun/preprocessing/oecd_airpollution.py
@@ -5,5 +5,7 @@ df = pd.read_csv("data/oecd.csv", low_memory=False)
 condition1 = df["Country"].isin(["United States", "Japan", "Germany", "United Kingdom", "France", "Italy", "Canada", "Korea", "Australia", "Spain"])
 condition2 = df["Year"] >= 1990
 condition3 = df["Year"] < 2020
-df = df.loc[condition1 & condition2 & condition3 , ["Country", "POL", "Year", "Value"]]
-df.to_csv("result/oecd-preprocessing-result.csv")
+condition4 = df["VAR"] == "TOT"
+df = df.loc[condition1 & condition2 & condition3 & condition4, ["Country", "POL", "Year", "Value"]]
+print(df)
+df.to_csv("result/oecd-result1.csv")
